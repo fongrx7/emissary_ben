f141967a8efb3eb4fbc00cebebb522b70b4f1cc7
Benjamin Kelm bekelm <bekelm@ip-10-113-15-173.evoforge.org>
2019-07-01 17:56:44 +0000

    CentOS 7 Jenkinsfile attempt #34

--- test_script.sh
+++ test_script.sh
@@ -4,7 +4,8 @@ timeout -s SIGINT 120s docker-compose up --force-recreate | tee test_results
 
 TEST_RESULTS=0
 
-TEST_VAL="$(more test_results | grep "filename: emissary-knight.png" | echo $?)"
+more test_results | grep "filename: emissary-knight.png" > /dev/null
+TEST_VAL="$(echo $?)"
 TEST_RESULTS=$(( $TEST_RESULT + $TEST_VAL))
 
 if [ "$TEST_RESULTS" -ne 0 ]
@@ -13,7 +14,8 @@ then
    exit 1
 fi
 
-TEST_VAL="$(more test_results | grep "filetype: PNG" | echo $?)"
+more test_results | grep "filetype: PNG" > /dev/null
+TEST_VAL="$(echo $?)"
 TEST_RESULTS=$(( $TEST_RESULT + $TEST_VAL))
 
 if [ "$TEST_RESULTS" -ne 0 ]
